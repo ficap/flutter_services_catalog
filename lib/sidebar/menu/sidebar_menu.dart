f4e0942f338da5799176f5d700c66484ba079d24
parchemaster <86682020+parchemaster@users.noreply.github.com>
2022-05-27 16:46:21 +0200

    finished edit by clicking on picture

--- lib/sidebar/menu/sidebar_menu.dart
+++ lib/sidebar/menu/sidebar_menu.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_image/firebase_image.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:services_catalog/authentication/authentification.dart';
+import 'package:services_catalog/di.dart';
 import 'package:services_catalog/entities/provider_model.dart';
 import 'package:services_catalog/my_color.dart';
 import 'package:services_catalog/sidebar/menu/head_item.dart';
@@ -20,77 +22,80 @@ class SideBarMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("rebuild sidebar");
-    final Widget then = providerModel != null
-        ? const UserProfilePage()
-        : const AuthenticationScreen();
 
     final String urlName = providerModel?.imagePath ?? "gs://" + FirebaseStorage.instance.ref().bucket + "/image_for_service_app/profile_image.png";
-    final image = FirebaseImage(urlName);
-
 
     return Drawer(
-      child: Material(
-        color: MyColor.backgroundColor,
-        child: ListView(
-          children: <Widget>[
-            Row(
-              children: [
 
-                Container(
-                  padding: padding,
-                  child: InkWell(
-                    child: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: FirebaseImage(urlName),
-                    ),
-                    // onTap: () => Navigator.of(context).push(
-                    //   MaterialPageRoute(
-                    //     builder: (context) => EditProfilePage(
-                    //       data: providerModel,
-                    //       imagePath: providerModel.imagePath,
-                    //     ),
-                    //   ),
-                    // ),
-                  ),
-                ),
+      child: Scaffold(
+          body: StreamBuilder<ProviderModel?>(
+              stream: Provider.of<DI>(context).currentUserStream,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData || snapshot.data == null) {
+                  return Container();
+                }
+                final providerModel = snapshot.data!;
 
-
-
-                InkWell(
-                  onTap: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return then;
-                      },
-                    ),
-                  ),
-                  child: Container(
-                    child: Row(
+                return ListView(
+                  children: <Widget>[
+                    Row(
                       children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              providerModel?.name ?? "Sign In",
-                              style: const TextStyle(fontSize: 20, color: Color.fromRGBO(93, 107, 89, 42)),
+                        Container(
+                          padding: padding,
+                          child: InkWell(
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundImage: FirebaseImage(urlName),
                             ),
-                            const SizedBox(height: 4),
-                            Text(
-                              providerModel?.email ?? "",
-                              style: const TextStyle(fontSize: 14, color: Color.fromRGBO(93, 107, 89, 42)),
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => EditProfilePage(
+                                  data: providerModel,
+                                  imagePath: providerModel.imagePath,
+                                ),
+                              ),
                             ),
-                          ],
+                          ),
                         ),
+
+
+
+                        InkWell(
+                          onTap: () => Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => UserProfilePage(
+                                providerModel: providerModel,
+                              ),
+                            ),
+                          ),
+                          child: Container(
+                            child: Row(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      providerModel?.name ?? "Sign In",
+                                      style: const TextStyle(fontSize: 20, color: Color.fromRGBO(93, 107, 89, 42)),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      providerModel?.email ?? "",
+                                      style: const TextStyle(fontSize: 14, color: Color.fromRGBO(93, 107, 89, 42)),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
                       ],
                     ),
-                  ),
-                )
-              ],
-            ),
-            if(providerModel != null)
-              LogOutItem(padding: padding)
-          ],
-        ),
+                    if(providerModel != null)
+                      LogOutItem(padding: padding)
+                  ],
+                );
+              })
       ),
     );
   }
